11fbcd518bd2a1fd8b45ab428e183f0c14c8527e
Ryan Resella <ryan@codeforamerica.org>
2011-03-31 20:20:14 -0700

    added relationships for solicitations

--- app/models/bidtype.rb
+++ app/models/bidtype.rb
@@ -1,4 +1,7 @@
 class Bidtype < ActiveRecord::Base
+  
+  has_many :solicitations
+  
    validates :abbreviation,  :presence => true
    validates :description,  :presence => true
 end
--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -1,6 +1,6 @@
 class Solicitation < ActiveRecord::Base
   
-  
+  belongs_to :bidtypes
   
    validates :bid_description,  :presence => true
 
