03565dc7d4633fee40071a98a0f6dcff13e20291
Ryan Resella <ryan@codeforamerica.org>
2011-03-31 17:59:56 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Purchasing::Application.config.secret_token = 'a33d0afa48f277b0290a5e753f848c5578369cb4e46a14977fccf709e2e0aef517369beb841703993d57d75c79d44bfb937f9a79fde94d2ac1480c537eb6f625'
