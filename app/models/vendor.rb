065fd9f86142a5256694b2d48dceb66036b73ade
Ryan Resella <ryan@codeforamerica.org>
2011-05-05 14:33:41 -0700

    Added validation to Vendors

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,2 +1,12 @@
 class Vendor < ActiveRecord::Base
+  
+  validates :name,  :presence => true
+  validates :company, :presence => true
+  validates :address, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
+  validates :zip, :presence => true
+  validates :phone, :presence => true
+  validates :email, :presence => true
+  
 end
